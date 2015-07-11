b4308dd75155b12b1749737d3f24ec715a2a0f50
rpvnwnkl <rpvnwnkl@gmail.com>
2015-07-10 22:09:30 -0400

    some mild cleanup, practicing vim

--- cleaner.py
+++ cleaner.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python
 from assembler import *
-##import time
-##import csv
 from matplotlib import pyplot
 import numpy
-
-##''' reads and re-interprets time sample '''
-##steve = time.strptime("30Nov2000", "%d%b%Y")
-##print ''.join(str([steve[2], steve[1], steve[0]]))
-##newSteve = time.strftime('%d%b%Y', steve)
-##print newSteve
 
 fileName='./foodList_x_test.csv'
 
@@ -28,6 +20,92 @@ food.save('foodList_x1_test.csv')
 '''prints out list of unique sellers'''
 ##for item in food.sellerList():
 ##    print item
+
+class Stats(object):
+    def __init__(self, selfObject):
+        self.Queue = selfObject
+
+    def monthlyTotal(self, beg=1, end=12):
+        '''returns dict of monthly totals'''
+        firstMonth = time.strftime("%B", time.strptime(str(beg), "%m"))
+        lastMonth = time.strftime("%B", time.strptime(str(end), "%m"))
+        monthTotals = {}
+        for item in self.Queue:
+            tmpMonth = item.Month()
+            if tmpMonth in range(beg, end+1):
+                if tmpMonth in monthTotals.keys():
+                    monthTotals[tmpMonth] += item.itemCost()
+                else:
+                    monthTotals[tmpMonth] = item.itemCost()
+                    
+        monthlyAVG = numpy.mean([monthTotals[x] for x in monthTotals])
+        print 'Monthly average purchase amount from {} through {} is: {}'.format( firstMonth, lastMonth, str(monthlyAVG))
+        
+        print 'Monthly totals for months '+firstMonth+' through '+lastMonth+'.'
+        for key in sorted(monthTotals.keys()):
+            print time.strftime("%b", time.strptime(str(key), "%m"))+': $'+str(monthTotals[key])
+        print '\n'
+        return monthTotals
+    
+    def typeTotals(self, beg, end):
+        '''returns dict of category totals per month'''
+         
+        firstMonth = time.strftime("%B", time.strptime(str(beg), "%m"))
+        lastMonth = time.strftime("%B", time.strptime(str(end), "%m"))
+        allMonths = {}
+        for item in self.Queue:
+            itemVal = item.getRowDict()
+            itemType = itemVal['Type']
+            itemCost = float(itemVal['Cost'])
+            tmpMonth = item.Month()
+            if tmpMonth in range(beg, end+1):##check if in desired range
+                if tmpMonth in allMonths.keys(): ##check if month already in dict
+                    if itemType in allMonths[tmpMonth]:##check if type is in month dict
+                        allMonths[tmpMonth][itemType] += itemCost ##add cost to type value in month dict
+                    else:
+                        allMonths[tmpMonth][itemType] = itemCost ##if not add it as origin value
+                else:                    
+                    allMonths[tmpMonth] = {itemType: itemCost} ##add month and add initial dict value
+                    
+        print 'Category totals for each month '+firstMonth+' through '+lastMonth+'.'
+
+        '''converting dict to list of lists'''
+        allInList = []
+        for key in sorted(allMonths):
+            monthList = []
+            for item in allMonths[key]:
+                monthList.append((item, allMonths[key][item]))
+            '''orders tuples by value size'''
+            allInList.append(sorted(monthList, key=lambda cost: cost[1], reverse=True))
+            
+        '''proceeds through and prints the list month by month'''
+        currentMonth = beg
+        for month in allInList:
+            print time.strftime("%b", time.strptime(str(currentMonth), "%m"))+':'
+            currentMonth += 1
+            for catVal in month:
+                print catVal[0]+': $'+str(catVal[1])
+            print '\n'
+            
+        return allInList
+facts = Stats(food)
+facts.monthlyTotal(1, 6)
+facts.typeTotals(1, 6)
+def monthlyGroup():
+    months = {}
+    for item in food.getFood():
+        tmpMonth = item.yMonth()
+        if tmpMonth in months.keys():
+            months[tmpMonth].append(item)
+        else:
+            months[tmpMonth] = [item]
+    return months
+##tmp = monthlyGroup()
+##for x in tmp.keys():
+##    for y in tmp[x]:
+##        print str(x)+': '+y.getName()
+
+##saveIt('foodList_a.csv')
 '''
 def straightPlot():
     dateArray = []
@@ -79,94 +157,3 @@ def straightPlot():
     pyplot.title('frequency of daily purchase amounts')
     pyplot.show()
     '''
-class Stats(object):
-    def __init__(self, selfObject):
-        self.Queue = selfObject
-
-    def monthlyTotal(self, beg=1, end=12):
-        '''returns dict of monthly totals'''
-        firstMonth = time.strftime("%B", time.strptime(str(beg), "%m"))
-        lastMonth = time.strftime("%B", time.strptime(str(end), "%m"))
-        monthTotals = {}
-        for item in self.Queue:
-            tmpMonth = item.Month()
-            if tmpMonth in range(beg, end+1):
-                if tmpMonth in monthTotals.keys():
-                    monthTotals[tmpMonth] += item.itemCost()
-                else:
-                    monthTotals[tmpMonth] = item.itemCost()
-                    
-        monthlyAVG = numpy.mean([monthTotals[x] for x in monthTotals])
-        print 'Monthly average purchase amount from {} through {} is: {}'.format( firstMonth, lastMonth, str(monthlyAVG))
-        
-        print 'Monthly totals for months '+firstMonth+' through '+lastMonth+'.'
-        for key in sorted(monthTotals.keys()):
-            print time.strftime("%b", time.strptime(str(key), "%m"))+': $'+str(monthTotals[key])
-        print '\n'
-        return monthTotals
-    
-    def typeTotals(self, beg, end):
-        '''returns dict of category totals per month'''
-         
-        firstMonth = time.strftime("%B", time.strptime(str(beg), "%m"))
-        lastMonth = time.strftime("%B", time.strptime(str(end), "%m"))
-        allMonths = {}
-        for item in self.Queue:
-            itemVal = item.getRowDict()
-            itemType = itemVal['Type']
-            itemCost = float(itemVal['Cost'])
-            tmpMonth = item.Month()
-            if tmpMonth in range(beg, end+1):##check if in desired range
-                if tmpMonth in allMonths.keys(): ##check if month already in dict
-                    if itemType in allMonths[tmpMonth]:##check if type is in month dict
-                        allMonths[tmpMonth][itemType] += itemCost ##add cost to type value in month dict
-                    else:
-                        allMonths[tmpMonth][itemType] = itemCost ##if not add it as origin value
-                else:                    
-                    allMonths[tmpMonth] = {itemType: itemCost} ##add month and add initial dict value
-                    
-##        print 'Category totals for each month '+firstMonth+' through '+lastMonth+'.'
-##        for key in sorted(allMonths):
-##            print time.strftime("%b", time.strptime(str(key), "%m"))+':'
-##            for itemtype in sorted(allMonths[key]):
-##                print itemtype+': $'+str(allMonths[key][itemtype])
-##            print '\n'
-        print 'Category totals for each month '+firstMonth+' through '+lastMonth+'.'
-
-        '''converting dict to list of lists'''
-        allInList = []
-        for key in sorted(allMonths):
-            monthList = []
-            for item in allMonths[key]:
-                monthList.append((item, allMonths[key][item]))
-            '''orders tuples by value size'''
-            allInList.append(sorted(monthList, key=lambda cost: cost[1], reverse=True))
-            
-        '''proceeds through and prints the list month by month'''
-        currentMonth = beg
-        for month in allInList:
-            print time.strftime("%b", time.strptime(str(currentMonth), "%m"))+':'
-            currentMonth += 1
-            for catVal in month:
-                print catVal[0]+': $'+str(catVal[1])
-            print '\n'
-            
-        return allInList
-facts = Stats(food)
-facts.monthlyTotal(1, 6)
-facts.typeTotals(1, 6)
-def monthlyGroup():
-    months = {}
-    for item in food.getFood():
-        tmpMonth = item.yMonth()
-        if tmpMonth in months.keys():
-            months[tmpMonth].append(item)
-        else:
-            months[tmpMonth] = [item]
-    return months
-##tmp = monthlyGroup()
-##for x in tmp.keys():
-##    for y in tmp[x]:
-##        print str(x)+': '+y.getName()
-
-##saveIt('foodList_a.csv')
