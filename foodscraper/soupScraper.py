4779b4aec7fbab8ae51ea5cd08729b3d6e132ef7
rpvnwnkl <rpvnwnkl@gmail.com>
2015-08-20 22:55:26 -0400

    not sure whats happening

--- foodscraper/soupScraper.py
+++ foodscraper/soupScraper.py
@@ -32,35 +32,101 @@ def printtag(soupTag):
 	print soupTag.string
 	print len(soupTag)
 	return
-url = 'http://www.hannaford.com/catalog/browse_products.cmd'
-soupFile = urllib.urlopen(url, 'r+')
+
+def getNode(nodeObj):
+    try:
+        ##print node
+        if 'id' in nodeObj.attrs:
+            tempID = nodeObj.attrs['id']
+            print tempID
+            return tempID.split('-')
+        else:
+            return
+    except:
+        return
+def inNodeDict(idString, nodeDict, subNodeDict):
+    if len(idString) == 0:
+        return nodeDict
+    elif idString[0] in subNodeDict:
+        subKey = idString[0]
+        idString.pop(0)
+        nodeDict[subKey] = inNodedict(idString, nodeDict, subNodeDict[subKey])
+    else:
+        subNodeDict[idString[0]] = {idString[0]:{}}
+        return subNodeDict
+
+##url = 'http://www.hannaford.com/catalog/browse_products.cmd'
+##soupFile = urllib.urlopen(url, 'r+')
+soupFile = open('page1File.txt', 'r')
 soup = BeautifulSoup(soupFile)
-printsoup(soup)
+##printsoup(soup)
 
 soupcopy = copy.copy(soup)
 
 for item in soup(class_='innerWrap'): ##Only works with one innerWrap div on page
-	innerWrap = item.extract() ##This pulls out the innerWrap object and renames it 
+	innerWrap = item.extract() ##This pulls out the innerWrap object and renames it i
+        print type(innerWrap)
 
 for item in innerWrap.children:
 	if type(item) == type(BeautifulSoup('<!-- -->').string): ##this finds children which are comments
-		item.extract() ##this gets rid of them
+            item.extract() ##this gets rid of them
 	else:
 		pass
 node01 = innerWrap.select('#node01-subNav')
 ##printsoup(innerWrap)
 ##printsoup(soup)
-printsoup(node01[0])
-print node01
-for x in node01: ##node01 is a list of the a single ul tag object, the main categories
-	printtag(x)
-	for xy in x('li'): ##this returns a list of all the li tag objects
-		print (xy)
-		print type(xy) 
-[printsoup(xy) for xy in node01[0]('li')] ##this will do nearly the same as above but return it all in a list
+##printsoup(node01[0])
+##print node01
+def clickNode(node, nodeDict = {}):
+    ##print type(node)
+    ##print len(node)
+    try:
+        printtag(node)
+        idList = getNode(node)
+        ##print node.attrs
+        if idList:
+            print inNodeDict(idList, nodeDict, nodeDict)
+
+            for x in range(len(idList)):
+                print x
+                
+                if idList[0] in nodeDict:
+                    if node.string:
+                        nodeDict[idList[x]].append(node.string)
+                else:
+                    if node.string:
+                        nodeDict[idList[x]] = [node.string]
+        for x in node:
+
+            print len(nodeDict)
+            clickNode(x, nodeDict)
+
+    except:
+        return nodeDict
+    '''if type(node) != type(list):
+        idList = getNode(node)
+        print idList
+        if idList:
+            
+            for x in node: ##node01 is a list of the a single ul tag object, the main categories
+##	        printtag(x)
+
+                return clickNode(x, nodeDict)
+        ##print node
+                ##for xy in x('li'): ##this returns a list of all the li tag objects
+		  ##  print (xy)
+		    ##print type(xy)
+     '''               ##print getNode(xy)
+    return nodeDict
+
+##print clickNode(node01)
+##print clickNode(soup
+print clickNode(innerWrap)
+##[printsoup(xy) for xy in node01[0]('li')] ##this will do nearly the same as above but return it all in a list
 ##page1File = open('page1File.txt', 'w')
 ##page1File.write(str(soup))
 ##page1File.close()
+soupFile.close()
 
 '''
 create tree of nodes
